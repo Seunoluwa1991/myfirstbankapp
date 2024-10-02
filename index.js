4ee03b5c0dbb6c4427293583a711364a559bfc1d
Seunoluwa1991 <kilashomichael@gmail.com>
2024-10-02 13:22:22 +0100

    First bankapp

---
+++ index.js
@@ -0,0 +1,50 @@
+
+function product(){
+   
+   const style = document.createElement("style");
+   style.innerHTML =
+   ` body{
+        background-color:black;
+   }
+   `
+//    " body{ background-color:black;}"
+   document.head.appendChild(style)
+}
+// function product(){
+
+//     // const toggleHight = document.getElementById("text");
+//     // toggleHight.classList.toggle("hightLight")
+//     const displayProduct = `
+//                     <div class=" ">
+//                     <ul>
+//                         <li> AgriBusiness </li>
+//                         <li> AgriLoans </li>
+//                         <li> Service </li>
+//                         <li> Service </li>
+//                     </ul>
+                   
+                    
+//                     </div>
+               
+//                 `;
+                
+                
+//                 document.getElementById("displayProduct").innerHTML = displayProduct;
+//                 document.getElementById("displayProduct").style = "border: solid 2px none;"
+//                 document.getElementById("displayProduct").style = "box-shadow: 2px 2px 2px 3px silver;"
+
+// }
+
+// function product(){
+//     const displayProduct = document.getElementById("displayProduct");
+//     displayProduct.style.backgroundColor = "red"
+//     displayProduct.style.border ="20px solid red"
+//     displayProduct.style.height ="200px"
+//     displayProduct.style.width ="120px"
+
+//     displayProduct.innerHTML =`
+//                 <option value ="Loan">Loan</li>
+//                 <option value ="Loan">Loan</li>
+//                 <option value ="Loan">Loan</li>
+//     `
+// }
